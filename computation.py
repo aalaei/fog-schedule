9eaeb79f19db70546447fa560b6507e2b94556d1
ali <ali.alaei.tabatabaei@gmail.com>
2021-06-16 18:48:36 +0430

    hash benchmark added

--- computation.py
+++ computation.py
@@ -1,6 +1,7 @@
 import hashlib
 import os
-
+import sys
+from time import time
 
 def try_num(problem_context, nonce, difficulty_level):
     sliced_none = []
@@ -8,7 +9,9 @@ def try_num(problem_context, nonce, difficulty_level):
         sliced_none.append(chr(nonce % 256))
         nonce = int(nonce // 256)
     new_str = problem_context.decode().replace("?", "".join(sliced_none))
-    return hashlib.sha256(new_str.encode()).hexdigest()[:difficulty_level] == ('0' * difficulty_level)
+    bin_hash_code =  hashlib.sha256(new_str.encode()).hexdigest()
+
+    return bin_hash_code[:difficulty_level] == ('0' * difficulty_level)
 
 
 def computational_task(name, difficulty_level):
@@ -20,3 +23,17 @@ def computational_task(name, difficulty_level):
         nonce += 1
     os.remove(name)
     return nonce
+
+
+if __name__ == '__main__':
+    t1=time()
+    if len(sys.argv)<2:
+        difficulty_level=5
+    else:
+        difficulty_level = eval(sys.argv[1])
+    problem_context ="seggseg?srgsdr".encode()
+    nonce = 1
+    while not try_num(problem_context, nonce, difficulty_level):
+        nonce += 1
+    t2=time()
+    print("passed time={}".format(t2-t1))
