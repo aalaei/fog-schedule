8b87345d29bcc0f4e89fdcb647d7fc7195d39ed7
ali <ali.alaei.tabatabaei@gmail.com>
2021-06-17 10:13:49 +0430

    added power to status class

--- status.py
+++ status.py
@@ -9,17 +9,19 @@ class Status:
             self.q_len = obj['q_len']
             self.cmp_cpcty = obj['cmp_cpcty']
             self.cmntn_rate = obj['cmntn_rate']
+            self.power = obj['power']
         else:
             self.id = -1
-            self.rssi = 1e-5
+            self.rssi = 0
             self.cmp_cpcty = 1e-5
             self.cmntn_rate = 1e-5
+            self.power = 1e-5
 
             self.q_len = 0
 
     def __str__(self):
-        return "id: {}, rssi: {:.2f}, q_len: {}, cmp_cpcty: {:.4f}, cmntn_rate: {:.2f} MBytes/s".format(
-            self.id, self.rssi, self.q_len, self.cmp_cpcty, self.cmntn_rate/1048576)
+        return "id: {}, rssi: {:.2f}, q_len: {}, cmp_cpcty: {:.4f}, cmntn_rate: {:.2f} MBytes/s, Power: {:.2f}W".format(
+            self.id, self.rssi, self.q_len, self.cmp_cpcty, self.cmntn_rate/1048576, self.power)
 
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__)
