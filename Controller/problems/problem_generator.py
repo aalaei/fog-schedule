def60508fb9a9664010dd9702f2ed3524659edc3
ali <ali.alaei.tabatabaei@gmail.com>
2021-06-24 11:12:02 +0430

    added random problem_generator

--- Controller/problems/problem_generator.py
+++ Controller/problems/problem_generator.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main(args):
-    transaction_count = 100_000
+    transaction_count = 1000_000
     problem_count = 10
     if len(args) > 2:
         transaction_count = eval(args[1])
@@ -16,7 +16,8 @@ def main(args):
     problems = ["problem" + str(x) + ".txt" for x in range(1, problem_count + 1)]
     for problem in problems:
         f = open(problem, "w")
-        for i in range(transaction_count):
+        random_d = random.randint(1, 100)
+        for i in range(transaction_count // random_d):
             f.write(chr(random.randint(ord('A'), ord('Z'))))
             # f.write(names.get_first_name())
             f.write(">")
